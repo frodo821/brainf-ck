1989d0dc44bf2467ecc8e2ab7b22d8703e39189a
frodo821 <1234567890.sakai.jp@gmail.com>
2017-07-23 23:32:37 +0900

    Update brainf*ck.py

--- brainf*ck.py
+++ brainf*ck.py
@@ -9,34 +9,46 @@ class Ptr:
         self.ptr = 0
         #internal memory
         self.stk = [0] * 3000
+    
     def increase(self):
         self.stk[self.ptr] += 1
+    
     def decrease(self):
         self.stk[self.ptr] -= 1
+    
     def increase_pointer(self):
         self.ptr += 1
         if self.ptr >= 3000:
             self.ptr -= 3000
+    
     def decrease_pointer(self):
         self.ptr -= 1
-        if self.ptr <= 3000
+        if self.ptr <= -3000:
             self.ptr += 3000
+    
     def get(self):
         self.stk[self.ptr] = ord(sys.stdin.read(1))
+    
     def put(self):
         print(chr(self.stk[self.ptr]), end='')
+    
     def Is_zero(self):
         return self.stk[self.ptr] == 0
+
 #char pointer in source code.
 scp = 0
+
 lblst = list()
+
 #nest count of square brankets.
 lbnest = 0
+
 try:
     src = open(sys.argv[1])
 except:
     print("cannot read file." ,file=sys.stderr)
     sys.exit(1)
+
 code = src.read()
 src.close()
 
@@ -47,22 +59,28 @@ while True:
         break
     if code[scp] == '>':
         stacks.increase_pointer()
+        scp += 1
     elif code[scp] == '<':
         stacks.decrease_pointer()
+        scp += 1
     elif code[scp] == '+':
         stacks.increase()
+        scp += 1
     elif code[scp] == '-':
         stacks.decrease()
+        scp += 1
     elif code[scp] == '.':
         stacks.put()
+        scp += 1
     elif code[scp] == ',':
         stacks.get()
+        scp += 1
     elif code[scp] == '[':
         if not scp in lblst:
             lblst.append(scp)
             lbnest += 1
         if not stacks.Is_zero():
-            pass
+            scp += 1
         else:
             while True:
                 scp += 1
@@ -71,6 +89,7 @@ while True:
                     break
     elif code[scp] == ']':
         scp = lblst[lbnest - 1]
+        del lblst[lbnest - 1]
         lbnest -= 1
     else:
         scp += 1
